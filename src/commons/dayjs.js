0ef1e91967caedcabe66128b3e1bba2dbc908153
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-09-24 09:00:05 +0000

    removed last bit of code, now it's really just a shell for dev deps.

--- src/commons/dayjs.js
+++
@@ -1,8 +0,0 @@
-const dayjs = require('dayjs')
-const utc = require('dayjs/plugin/utc')
-// const timezone = require('dayjs/plugin/timezone')
-
-dayjs.extend(utc)
-// dayjs.extend(timezone)
-
-module.exports = {dayjs}
--- src/commons/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = {dayjs: () => require('./dayjs')}
